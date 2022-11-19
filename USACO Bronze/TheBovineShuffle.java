672d23d68cddbbfeb052f512bf5aa441e1eef08d
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2022-11-18 17:16:04 -0800

    Update and rename Problem12TheBovineShuffle.java to TheBovineShuffle.java

--- USACO Bronze/TheBovineShuffle.java
+++ USACO Bronze/TheBovineShuffle.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Problem12TheBovineShuffle {
+public class TheBovineShuffle {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new File("shuffle.in"));
