e466d87b15a68ab426e022c965c8db65b78259c9
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2022-11-21 14:17:05 -0800

    Update and rename Problem52BlockGame.java to BlockGame.java

--- USACO Bronze/BlockGame.java
+++ USACO Bronze/BlockGame.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Problem52BlockGame {
+public class BlockGame {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new File("blocks.in"));
