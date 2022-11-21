09b8f3ef0d2c3a025dc570fd42053b70a78650f2
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2022-11-20 23:12:59 -0800

    Update and rename Problem34DontBeLast.java to DontBeLast.java

--- USACO Bronze/DontBeLast.java
+++ USACO Bronze/DontBeLast.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Problem34DontBeLast {
+public class DontBeLast {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new File("notlast.in"));
