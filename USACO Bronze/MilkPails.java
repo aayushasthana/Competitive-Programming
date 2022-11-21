21764b983620044f679dd0dd6b7fd99243ea18e3
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2022-11-20 20:02:58 -0800

    Update and rename Problem24MilkPails.java to MilkPails.java

--- USACO Bronze/MilkPails.java
+++ USACO Bronze/MilkPails.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Problem24MilkPails {
+public class MilkPails {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new File("pails.in"));
