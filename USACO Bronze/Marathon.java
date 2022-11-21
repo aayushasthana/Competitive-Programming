3b6723a849abc79f60c35b290712189c61eb52c6
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2022-11-21 14:19:37 -0800

    Update and rename Problem68Marathon.java to Marathon.java

--- USACO Bronze/Marathon.java
+++ USACO Bronze/Marathon.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Problem68Marathon {
+public class Marathon {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new File("marathon.in"));
