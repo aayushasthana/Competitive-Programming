f3bd1ea4f3bf15b8a2fddae72de1495177623f82
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2022-11-20 23:10:09 -0800

    Update and rename Problem36SocialDistancingII.java to SocialDistancing2.java

--- USACO Bronze/SocialDistancing2.java
+++ USACO Bronze/SocialDistancing2.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Problem36SocialDistancingII {
+public class SocialDistancing2 {
 
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new File("socdist2.in"));
